b29747dcf71cac105161e2a93890562ea43e0236
Christopher Wagner <christopher.wagner@secured.team>
2024-08-21 14:40:28 -0400

    clean up bin

--- bin/servicecatalog-app.ts
+++ bin/servicecatalog-app.ts
@@ -1,6 +1,5 @@
 import * as cdk from 'aws-cdk-lib';
 import { ServicecatalogStack } from '../lib/servicecatalog-stack';
-// import { VpcProduct } from '../lib/products/vpc-product-stack';
 
 const app = new cdk.App();
 
